e41ca8bb3054cb4e83947f2bc6ff4245f493e476
talonmyburgh <talon.myburgh@mydon.co.za>
2022-11-16 09:12:24 +0000

    ** logger and calibration delay pub simplified

--- delay_logger.py
+++ delay_logger.py
@@ -1,10 +1,7 @@
-from cosmic.fengines import ant_remotefeng_map
-from cosmic.redis_actions import redis_obj, redis_hget_keyvalues, redis_publish_dict_to_hash, redis_publish_service_pulse
+from cosmic.redis_actions import redis_obj, redis_publish_service_pulse
 import time
 import logging
-import math
 from logging.handlers import RotatingFileHandler
-import redis
 import os
 import argparse
 
@@ -42,110 +39,18 @@ class DelayLogger:
     def __init__(self, redis_obj, polling_rate):
         self.redis_obj = redis_obj
         self.polling_rate = polling_rate
-        self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(
-            self.redis_obj
-        )
         
         logger.info("Starting Delay logger...\n")
 
-    def _fetch_model_values(self):
-        """
-        Fetch the redis delays from META_modelDelays. We don't care about calibration
-        delays since we are purely comparing the slope of the delays which 
-        are rate dependant
-        TODO: predict and verify loaded phase
-        
-        :return: a mapping of antenna to delay rate values
-        :rtype: Dict
-        """
-
-        modelDelays = redis_hget_keyvalues(self.redis_obj, "META_modelDelays")
-        onesec_future_integer = int(time.time() + 1) #1 second into the future
-        ant2delayrate = {}
-        for ant, value in modelDelays.items():
-            if isinstance(value, dict):
-                loadtime_diff_modeltime = (onesec_future_integer - value.get("time_value", 0.0)) #calculate 1s into the future
-                delay_raterate = value.get("delay_raterate_nsps2", 0.0)
-                delay_rate = value.get("delay_rate_nsps", 0.0)
-
-                # dT/dt = 2ax + b
-                model_delay_rate = delay_raterate * 2 * loadtime_diff_modeltime + delay_rate
-                logger.info(f"Model delays received for antenna {ant}: {modelDelays[ant]}")
-                logger.info(f"Calculated expected delay rate of {model_delay_rate} for antenna {ant}.")
-                ant2delayrate[ant] = model_delay_rate
-        sleep_time = onesec_future_integer - time.time()
-        sleep_time = -1 * sleep_time if sleep_time < 0 else sleep_time
-        time.sleep(sleep_time)
-        return ant2delayrate
-    
-    def _fetch_feng_state(self, antname):
-        """
-        Get the delay state for the F-Engine instance attached to antname.
-        
-        :param antname: Name of the antenna from which to collect delay state.
-        :type antname: str
-
-        :return: dictionary indicating tracking on/off and state of delay tracking
-        :rtype: {"on" : bool, "tracking" : bool}
-        """
-        feng_obj = self.ant_feng_map[antname]
-        delay_state = {
-            "on" : feng_obj.delay_switch.is_set(),
-            "tracking" : feng_obj.delay_track.is_set()
-        }
-        logger.info(f"Antenna {antname} has delay state {delay_state}.")
-        return delay_state
-
-    def _fetch_feng_values(self, antname):
-        """
-        Given an antenna name, fetch the firmware reported phase
-        and slope and check they match with the values received 
-        by redis channel.
-
-        :param antname: Name of the antenna from which to collect slope and phase.
-        :type antname: str
-
-        :return: firmware slopes and firmware phases of the antenna
-        :rtype: tuple - slope,phase
-        """
-        feng_obj = self.ant_feng_map[antname]
-        slope, s_scale = feng_obj.phaserotate.get_firmware_slope(0)
-        phase, p_scale = feng_obj.phaserotate.get_firmware_phase(0)
-        return slope/s_scale, phase/p_scale    
-
     def run(self):
         """
-        Every polling rate, for every antenna, listen for the 
-        delays broadcast, and when received, compare against the loaded values."""
-        self.ant2delay_log = {}
-
+        Every polling rate, fetch from FENG_delayStatus the entire dict
+        and log it to file.
+        """
         while True:
             redis_publish_service_pulse(self.redis_obj, SERVICE_NAME)
-            ant2delayrate = self._fetch_model_values()
-            
-            for ant, delay_rate in ant2delayrate.items():
-                logger.info(f"Checking delay status of antenna {ant}")
-                feng_state = self._fetch_feng_state(ant)
-                slope, phase = self._fetch_feng_values(ant)
-                logger.info(f"Received slope = {slope} and phase = {phase} for antenna {ant}.")
-
-                if feng_state["on"] and feng_state["tracking"]:
-                    #thread is live and tracking, check against model
-                    logger.info("Delay thread is live and delays are tracking.")
-                    is_correct = math.isclose(slope, delay_rate, rel_tol=2**-18)
-                    feng_state["correct"] = is_correct
-                    if not is_correct:
-                        logging.error(f"Firmware reports slope = {slope} which is not close to model value = {delay_rate}.")
-                else:
-                    logger.info("Delay thread is dead")
-                    #calibration mode or delay thread is dead, check against 0
-                    is_correct = math.isclose(slope, 0.0, rel_tol=2**-18)
-                    feng_state["correct"] = is_correct
-                    if not is_correct:
-                        logging.error(f"Firmware reports slope = {slope} which is not close to model value = {delay_rate}.")
-
-                self.ant2delay_log[ant] = feng_state
-            redis_publish_dict_to_hash(self.redis_obj, "FENG_delayStatus", self.ant2delay_log)
+            str_to_log = self.redis_obj.hgetall("FENG_delayStatus")
+            logger.info(str_to_log)
 
             time.sleep(self.polling_rate)
 
--- delaycalibration.py
+++ delaycalibration.py
@@ -1,87 +1,47 @@
 import pandas as pd
 import numpy as np
-import threading
-import time
-import logging
-from logging.handlers import RotatingFileHandler
 import argparse
 import astropy.constants as const
 import os
-from cosmic.redis_actions import redis_obj, redis_publish_service_pulse, redis_publish_dict_to_hash, redis_publish_dict_to_channel
-
-#LOGGING
-LOGFILENAME = "/home/cosmic/logs/DelayCalibrations.log"
-
-SERVICE_NAME = os.path.splitext(os.path.basename(__file__))[0]
-
-#LOGGER:
-logger = logging.getLogger('calibration_delays')
-logger.setLevel(logging.INFO)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-fh = RotatingFileHandler(LOGFILENAME, mode = 'a', maxBytes = 512, backupCount = 1, encoding = None, delay = False)
-fh.setLevel(logging.INFO)
-
-# create formatter
-formatter = logging.Formatter("[%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s] %(message)s")
-
-# add formatter to ch
-ch.setFormatter(formatter)
-fh.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-logger.addHandler(fh)
+from cosmic.redis_actions import redis_obj, redis_publish_dict_to_hash
 
 #CONSTANTS
 ADVANCE_TIME = (8e3/const.c.value) #largest baseline 8km / c ~ largest calibration delay in s
 
-class DelayCalibration(threading.Thread):
-    def __init__(self, redis_obj, calib_csv, polling_rate):
+class DelayCalibration():
+    def __init__(self, redis_obj, calib_csv):
         """
         A delay calibration calculation object.
         Makes use of observation recordings
         to calculate the calibration delays for each stream
         of each antenna and sends them 
-        to the fengine nodes via redis channels.
+        to the redis hash META_calibrationDelays.
         """
-        threading.Thread.__init__(self)
-
         self.redis_obj = redis_obj
-        self.polling_rate = polling_rate
 
         #initialise calibration delay data dictionary
         self.ant2calibmap_init = pd.read_csv(os.path.abspath(calib_csv), names = ["IF0","IF1","IF2","IF3"], header=None, skiprows=1).to_dict('index')
-        logger.info(f"Read in the following calibration dict:\n{self.ant2calibmap_init}")
         self.ant2calibmap = {}
 
     def run(self):
         """
-        Started in a thread, this function will take the calibration delay values.
-        These values add/subtract from the delay values on the F-Engine.
+        This function will take the calibration delay values in conjunction with ADVANCE_TIME
+        and publish them to redis hash META_calibrationDelays.
         """
-        while True:
-            for ant, calib_value in self.ant2calibmap_init.items():
-                values = np.fromiter(calib_value.values(),dtype=float)
-                values = values + (ADVANCE_TIME* 1e9)
-                tmp_calib_values = {}
-                for i, key in enumerate(calib_value):
-                    tmp_calib_values[key] = values[i]
-                self.ant2calibmap[ant] = tmp_calib_values
-            redis_publish_service_pulse(self.redis_obj, SERVICE_NAME)
-            self.publish_calibration_delays()
-            time.sleep(self.polling_rate)
+        for ant, calib_value in self.ant2calibmap_init.items():
+            values = np.fromiter(calib_value.values(),dtype=float)
+            values = values + (ADVANCE_TIME* 1e9)
+            tmp_calib_values = {}
+            for i, key in enumerate(calib_value):
+                tmp_calib_values[key] = values[i]
+            self.ant2calibmap[ant] = tmp_calib_values
+        self.publish_calibration_delays()
 
     def publish_calibration_delays(self):
         """
         Push out calibration delay values on individual channels for each antenna.
         Then push out the full dictionary to a hash for display purposes.
         """
-        for ant, calib_delay in self.ant2calibmap.items():
-            redis_publish_dict_to_channel(self.redis_obj, f"{ant}_calibration_delays", calib_delay)
-        logger.info(f"Published calibration delay dictionary:\n{self.ant2calibmap}")
         redis_publish_dict_to_hash(self.redis_obj, "META_calibrationDelays", self.ant2calibmap)
 
 if __name__ == "__main__":
@@ -89,18 +49,7 @@ if __name__ == "__main__":
     description=("Set up the Delay logger.")
     )
     parser.add_argument("calibration_csv", type=str, help="calibration.csv filepath.")
-    parser.add_argument(
-    "-p","--polling_rate", type=float, help="Rate at which delays are publish.", default=5
-    )
-    parser.add_argument(
-    "-c", "--clean", action="store_true",help="Delete the existing log file and start afresh.",
-    )
     args = parser.parse_args()
-    if os.path.exists(LOGFILENAME) and args.clean:
-        logging.info("Removing previous log file...")
-        os.remove(LOGFILENAME)
-    else:
-        logging.info("Nothing to clean, continuing...")
-    delayCalibration = DelayCalibration(redis_obj, args.calibration_csv, args.polling_rate)
-    logger.info("Calibration delay object instantiated, running now...")
+    
+    delayCalibration = DelayCalibration(redis_obj, args.calibration_csv)
     delayCalibration.run()
