ae4cdd8a51bb1385224ff034b257f62c7f123832
talonmyburgh <talon.myburgh@mydon.co.za>
2023-01-26 11:09:36 +0000

    ** dont die on remoteobject respawn. ++ check phase calibration values on F-Engines

--- delay_logger.py
+++ delay_logger.py
@@ -1,6 +1,7 @@
 from cosmic.redis_actions import redis_obj, redis_publish_service_pulse, redis_hget_keyvalues, redis_publish_dict_to_hash
 from cosmic.fengines import ant_remotefeng_map
 import time
+import numpy as np
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -53,27 +54,51 @@ class DelayLogger:
         while True:
             redis_publish_service_pulse(self.redis_obj, SERVICE_NAME)
             delay_status_dict = redis_hget_keyvalues(self.redis_obj, "FENG_delayStatus")
+            ant_residual_phase_dict = redis_hget_keyvalues(self.redis_obj, "META_residualPhases")
             for ant, feng in self.ant_feng_map.items():
-                if ant in delay_status_dict:
-                    delay_dict = delay_status_dict[ant]
-                    delay_dict["on"] = str(feng.delay_switch.is_set())
-                    if (feng.delay_track.is_set() and not feng.delay_halfoff.is_set() and not feng.delay_halfcal.is_set()
-                        and not feng.delay_halfphase.is_set() and not feng.delay_halfphasecorrection.is_set()):
-                        delay_dict["tracking"] = "True"
-                    elif (feng.delay_track.is_set() and feng.delay_halfcal.is_set()):
-                        delay_dict["tracking"] = "half-cal"
-                    elif (feng.delay_track.is_set() and feng.delay_halfoff.is_set()):
-                        delay_dict["tracking"] = "half-off"
-                    elif (feng.delay_track.is_set() and feng.delay_halfphase.is_set()):
-                        delay_dict["tracking"] = "half-phase"
-                    elif (feng.delay_track.is_set() and feng.delay_halfphasecorrection.is_set()):
-                        delay_dict["tracking"] = "half-corrected-phase"
-                    else:
-                        delay_dict["tracking"] = "fixed-only"
+                try:
+                    if ant in delay_status_dict:
+                        delay_dict = delay_status_dict[ant]
+                        feng_delay_status = feng.get_status_delay_tracking()
+                        delay_dict["ok"] = str(feng_delay_status["ok"])
+                        delay_dict["on"] = str(feng_delay_status["is_alive"])
+                        if (feng.delay_track.is_set() and not feng.delay_halfoff.is_set() and not feng.delay_halfcal.is_set()
+                            and not feng.delay_halfphase.is_set() and not feng.delay_halfphasecorrection.is_set()):
+                            delay_dict["tracking"] = "True"
+                        elif (feng.delay_track.is_set() and feng.delay_halfcal.is_set()):
+                            delay_dict["tracking"] = "half-cal"
+                        elif (feng.delay_track.is_set() and feng.delay_halfoff.is_set()):
+                            delay_dict["tracking"] = "half-off"
+                        elif (feng.delay_track.is_set() and feng.delay_halfphase.is_set()):
+                            delay_dict["tracking"] = "half-phase"
+                        elif (feng.delay_track.is_set() and feng.delay_halfphasecorrection.is_set()):
+                            delay_dict["tracking"] = "half-corrected-phase"
+                        elif (feng.delay_track.is_set() and feng.delay_fullphasecorrection.is_set()):
+                            delay_dict["tracking"] = "full-corrected-phase"
+                        else:
+                            delay_dict["tracking"] = "fixed-only"
 
-                    new_delay_status_dict[ant] = delay_dict
-                else:
-                    new_delay_status_dict[ant] = f"No delay status available for {ant}..."
+
+                        #Check phase_calibration values against META_residualPhases:
+                        phase_correct = []
+                        for stream in range(4):
+                            if ant in ant_residual_phase_dict:
+                                expected_residual_phase = (np.array(ant_residual_phase_dict[ant][stream],dtype=float) + np.pi) % (2 * np.pi) - np.pi
+                                phase_correct += [bool(np.all(np.isclose(expected_residual_phase,
+                                                    np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
+                            else:
+                                phase_correct += [bool(np.all(np.isclose(np.array([0.0]*1024,dtype=float),
+                                                    np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
+
+                        delay_dict["phase_cal_correct"] = phase_correct
+                        new_delay_status_dict[ant] = delay_dict
+                    else:
+                        new_delay_status_dict[ant] = f"No delay status available for {ant}..."
+                except:
+                    print(f"Could not reach antenna {ant}")
+                    new_delay_status_dict[ant] = f"Unable to reach {ant}. F-Engine may be unreachable."
+                    self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(self.redis_obj)
+                    continue
 
             logger.info(f"Delay state: {new_delay_status_dict}")
             redis_publish_dict_to_hash(self.redis_obj, "FENG_delayState", new_delay_status_dict)
