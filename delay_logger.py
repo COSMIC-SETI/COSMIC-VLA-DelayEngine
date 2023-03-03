82a0c451d081d6542f390c093d9f2a498b302cc8
talonmyburgh <talon.myburgh@mydon.co.za>
2023-03-03 09:49:47 +0000

    ** determine fshifts loaded and used are correct

--- delay_logger.py
+++ delay_logger.py
@@ -1,5 +1,6 @@
 from cosmic.redis_actions import redis_obj, redis_publish_service_pulse, redis_hget_keyvalues, redis_publish_dict_to_hash
 from cosmic.fengines import ant_remotefeng_map
+from cosmic.fengines import delays, configure
 import time
 import numpy as np
 import logging
@@ -31,6 +32,78 @@ fh.setFormatter(formatter)
 logger.addHandler(ch)
 logger.addHandler(fh)
 
+def fetch_delay_status_dict(redis_obj, ant_feng_map):
+    """
+    Function collects from Redis hashes and over remoteobjects from the F-Engines a 
+    status dictionary that it will return.
+
+    Args:
+        redis_obj object: an appropriate redis object that contains relevant hashes
+        ant_feng_map dict: mapping of antenna names to cosmic_feng objects
+
+    Returns:
+        delay_dict dict: {ant:delay_dict} where delay_dict is a dictionary containing many fields detailing
+                        the overall state of the F-Engine tracking, fshift and calibration state
+        bad_ant_list: a list of F-Engines that are unreachable
+    """
+    ant_delay_status_dict = {}
+    antenna = list(ant_feng_map.keys())
+    delay_status_dict = redis_hget_keyvalues(redis_obj, "FENG_delayStatus", keys = antenna)
+    ant_residual_phase_dict = redis_hget_keyvalues(redis_obj, "META_calibrationPhases", keys = antenna)
+    antname_lo_fshift_dict = delays.get_antToFshiftMap(
+            redis_obj,
+            ["A", "B", "C", "D"],
+            delays.get_sideband(redis_obj),
+            antenna,
+        )
+    bad_ant_list = []
+    for ant, feng in ant_feng_map.items():
+        try:
+            if ant in delay_status_dict:
+                delay_dict = delay_status_dict[ant]
+                feng_delay_status = feng.get_status_delay_tracking()
+                delay_dict["ok"] = str(feng_delay_status["ok"])
+                delay_dict["on"] = str(feng_delay_status["is_alive"])
+                if (feng.delay_track.is_set() and not feng.delay_halfoff.is_set() and not feng.delay_halfcal.is_set()
+                    and not feng.delay_halfphase.is_set() and not feng.delay_halfphasecorrection.is_set()):
+                    delay_dict["tracking"] = "True"
+                elif (feng.delay_track.is_set() and feng.delay_halfcal.is_set()):
+                    delay_dict["tracking"] = "half-cal"
+                elif (feng.delay_track.is_set() and feng.delay_halfoff.is_set()):
+                    delay_dict["tracking"] = "half-off"
+                elif (feng.delay_track.is_set() and feng.delay_halfphase.is_set()):
+                    delay_dict["tracking"] = "half-phase"
+                elif (feng.delay_track.is_set() and feng.delay_halfphasecorrection.is_set()):
+                    delay_dict["tracking"] = "half-corrected-phase"
+                elif (feng.delay_track.is_set() and feng.delay_fullphasecorrection.is_set()):
+                    delay_dict["tracking"] = "full-corrected-phase"
+                else:
+                    delay_dict["tracking"] = "fixed-only"
+
+                #Check phase_calibration values against META_residualPhases:
+                phase_correct = []
+                feng_fshifts = np.round(delay_dict["loaded_fshift_hz"]).tolist()
+                expected_fshifts = np.round(configure.order_lo_dict_values(antname_lo_fshift_dict[ant])).tolist()
+                for stream in range(4):
+                    if ant in ant_residual_phase_dict:
+                        expected_residual_phase = (np.array(ant_residual_phase_dict[ant][stream],dtype=float) + np.pi) % (2 * np.pi) - np.pi
+                        phase_correct += [bool(np.all(np.isclose(expected_residual_phase,
+                                            np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
+                    else:
+                        phase_correct += [bool(np.all(np.isclose(np.array([0.0]*1024,dtype=float),
+                                            np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
+                delay_dict["expected_fshift_hz"] = expected_fshifts
+                delay_dict["fshifts_correct"] = feng_fshifts == expected_fshifts
+                delay_dict["phase_cal_correct"] = phase_correct
+                ant_delay_status_dict[ant] = delay_dict
+            else:
+                 ant_delay_status_dict[ant] =  f"No delay status available for {ant}..."
+        except:
+            ant_delay_status_dict[ant] = f"Unable to reach {ant}. F-Engine may be unreachable."
+            bad_ant_list += [ant]
+            continue
+    return ant_delay_status_dict, bad_ant_list
+
 class DelayLogger:
     """
     Every period, listen for broadcast delays. Calculate expected loaded delays/phases,
@@ -41,69 +114,29 @@ class DelayLogger:
     def __init__(self, redis_obj, polling_rate):
         self.redis_obj = redis_obj
         self.polling_rate = polling_rate
-        self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(self.redis_obj)
-        
+        self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(redis_obj)
         logger.info("Starting Delay logger...\n")
-
+    
     def run(self):
         """
-        Every polling rate, fetch from FENG_delayStatus the entire dict
-        and ammend it to account for the state of the delay threads before publishing.
+        Every polling rate, fetch from fetch_delay_status_dict() an antenna:delaydict mapping 
+        to give an indication of the correctness of tracking.
         """
-        new_delay_status_dict = {}
+        i = 0
         while True:
-            redis_publish_service_pulse(self.redis_obj, SERVICE_NAME)
-            delay_status_dict = redis_hget_keyvalues(self.redis_obj, "FENG_delayStatus")
-            ant_residual_phase_dict = redis_hget_keyvalues(self.redis_obj, "META_calibrationPhases")
-            for ant, feng in self.ant_feng_map.items():
-                try:
-                    if ant in delay_status_dict:
-                        delay_dict = delay_status_dict[ant]
-                        feng_delay_status = feng.get_status_delay_tracking()
-                        delay_dict["ok"] = str(feng_delay_status["ok"])
-                        delay_dict["on"] = str(feng_delay_status["is_alive"])
-                        if (feng.delay_track.is_set() and not feng.delay_halfoff.is_set() and not feng.delay_halfcal.is_set()
-                            and not feng.delay_halfphase.is_set() and not feng.delay_halfphasecorrection.is_set()):
-                            delay_dict["tracking"] = "True"
-                        elif (feng.delay_track.is_set() and feng.delay_halfcal.is_set()):
-                            delay_dict["tracking"] = "half-cal"
-                        elif (feng.delay_track.is_set() and feng.delay_halfoff.is_set()):
-                            delay_dict["tracking"] = "half-off"
-                        elif (feng.delay_track.is_set() and feng.delay_halfphase.is_set()):
-                            delay_dict["tracking"] = "half-phase"
-                        elif (feng.delay_track.is_set() and feng.delay_halfphasecorrection.is_set()):
-                            delay_dict["tracking"] = "half-corrected-phase"
-                        elif (feng.delay_track.is_set() and feng.delay_fullphasecorrection.is_set()):
-                            delay_dict["tracking"] = "full-corrected-phase"
-                        else:
-                            delay_dict["tracking"] = "fixed-only"
-
-
-                        #Check phase_calibration values against META_residualPhases:
-                        phase_correct = []
-                        for stream in range(4):
-                            if ant in ant_residual_phase_dict:
-                                expected_residual_phase = (np.array(ant_residual_phase_dict[ant][stream],dtype=float) + np.pi) % (2 * np.pi) - np.pi
-                                phase_correct += [bool(np.all(np.isclose(expected_residual_phase,
-                                                    np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
-                            else:
-                                phase_correct += [bool(np.all(np.isclose(np.array([0.0]*1024,dtype=float),
-                                                    np.array(feng.phaserotate.get_phase_cal(stream),dtype=float), atol=1e-1)))] 
-
-                        delay_dict["phase_cal_correct"] = phase_correct
-                        new_delay_status_dict[ant] = delay_dict
-                    else:
-                        new_delay_status_dict[ant] = f"No delay status available for {ant}..."
-                except:
-                    print(f"Could not reach antenna {ant}")
-                    new_delay_status_dict[ant] = f"Unable to reach {ant}. F-Engine may be unreachable."
-                    self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(self.redis_obj)
-                    continue
-
-            logger.info(f"Delay state: {new_delay_status_dict}")
-            redis_publish_dict_to_hash(self.redis_obj, "FENG_delayState", new_delay_status_dict)
-
-            time.sleep(self.polling_rate)
+            if i > 20 and len(bad_ant_list) != 0:
+                self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(redis_obj)
+                i = 0
+            else:
+                redis_publish_service_pulse(self.redis_obj, SERVICE_NAME)
+                t = time.time()
+                ant_delay_status_dict, bad_ant_list = fetch_delay_status_dict(self.redis_obj, self.ant_feng_map)
+                duration = time.time() - t
+                print(f"Duration = {duration}s")
+                logger.info(f"Delay state: {ant_delay_status_dict}")
+                redis_publish_dict_to_hash(self.redis_obj, "FENG_delayState", ant_delay_status_dict)
+                time.sleep(self.polling_rate - duration if duration < self.polling_rate else 0.0)
+                i+=1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
