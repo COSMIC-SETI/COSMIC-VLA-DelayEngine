03591f0d6e270e7af2a4db6be0200379220a218e
talonmyburgh <talon.myburgh@mydon.co.za>
2023-04-05 13:19:18 +0000

    *+ slack configuration message

--- configure_calibration_process.py
+++ configure_calibration_process.py
@@ -2,12 +2,14 @@ from cosmic.redis_actions import redis_obj, redis_publish_dict_to_hash
 from delaycalibration import load_delay_calibrations
 from phasecalibration import load_phase_calibrations
 from calibration_gain_collator import CONFIG_HASH
+from cosmic.observations.slackbot import SlackBot
 import argparse
+import json
 import os
 
 def load_and_configure_calibrations(user_output_dir=None, hash_timeout=20, re_arm_time = 30, fit_method = "linear",
                                     input_fixed_delays = "fixed_delay_init.csv", input_fixed_phases = "fixed_phases_init.json",
-                                    snr_threshold = 4.0):
+                                    snr_threshold = 4.0, slackbot = None):
     user_output_dir = os.path.abspath(user_output_dir)
     input_fixed_delays = os.path.abspath(input_fixed_delays)
     input_fixed_phases = os.path.abspath(input_fixed_phases)
@@ -23,6 +25,11 @@ def load_and_configure_calibrations(user_output_dir=None, hash_timeout=20, re_ar
     redis_publish_dict_to_hash(redis_obj, CONFIG_HASH, config_dict)
     load_delay_calibrations(input_fixed_delays)
     load_phase_calibrations(input_fixed_phases)
+    msg = f"""
+    Reconfiguring calibration process with following configuration:
+    ```{config_dict}```"""
+    print(msg)
+    slackbot.post_message(msg)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -43,11 +50,18 @@ if __name__ == "__main__":
     parser.add_argument("-p","--fixed-phase-to-update", type=str, required=False, help="""
     json file path to latest fixed phases that must be modified by the residual phases calculated in this script. If not provided,
     process will try use fixed-phase file path in cache.""")
+    parser.add_argument("--no-slack-post", action="store_true",help="""If specified, logs are not posted to slack.""")
     parser.add_argument("--snr-threshold", type=float, default = 4.0, required=False, 
                         help="""The snr threshold above which the process will reject applying the calculated delay
                         and phase residual calibration values""")
     args = parser.parse_args()
+
+    slackbot = None
+    if not args.no_slack_post:
+        if "SLACK_BOT_TOKEN" in os.environ:
+            slackbot = SlackBot(os.environ["SLACK_BOT_TOKEN"], chan_name="active_vla_calibrations", chan_id="C04KTCX4MNV")
+
     load_and_configure_calibrations(user_output_dir=args.output_dir, hash_timeout=args.hash_timeout, re_arm_time=args.re_arm_time,
                                     fit_method=args.fit_method, input_fixed_delays=args.fixed_delay_to_update,
-                                    input_fixed_phases=args.fixed_phase_to_update, snr_threshold=args.snr_threshold)
+                                    input_fixed_phases=args.fixed_phase_to_update, snr_threshold=args.snr_threshold, slackbot=slackbot)
     
