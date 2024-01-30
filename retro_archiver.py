edc093b519c7cec407da59e9823cb6f0ddc54f25
talonmyburgh <talon.myburgh@mydon.co.za>
2024-01-30 09:55:58 +0000

    ++ add logging

--- retro_archiver.py
+++ retro_archiver.py
@@ -17,6 +17,8 @@ logger.setLevel(logging.DEBUG)
 fh = WatchedFileHandler(LOGFILENAME, mode = 'a')
 fh.setLevel(logging.DEBUG)
 
+logger.addHandler(fh)
+
 def UnixTimeFromModifiedJulianDate(jd):
     return (jd-40587)*86400000
 
@@ -93,39 +95,38 @@ if __name__ == "__main__":
             delays_calculated = os.path.isdir(root + "/calibration/fixed_delays") and len(os.listdir(root + "/calibration/fixed_delays"))==1
             phases_calculated = os.path.isdir(root + "/calibration/fixed_phases") and len(os.listdir(root + "/calibration/fixed_phases"))==1
             if delays_calculated and phases_calculated:
-                print(f"Processing contents of directory {root} as there are calibration products in here")
+                logger.info(f"Processing contents of directory {root} as there are calibration products in here")
             else:
-                print(f"Not processing contents of directory {root} as there are no calibration products in here")
-                #LOG TO FILE THAT THIS CAN POTENTIALLY BE DELETED
+                logger.error(f"Not processing contents of directory {root} as there are no/partial calibration products in here.\nCould delete this calibration folder")
                 continue
 
             #Check uvh5 files present in root
             if not any(file.endswith('.uvh5') for file in os.listdir(root)):
-                print(f"No UVH5 files present in scan root directory: {root}")
+                logger.warning(f"No UVH5 files present in scan root directory: {root}.\nCalibration products exist but no UVH5 files present.")
                 no_uvh5 = True
-                #LOG TO FILE THAT CALIBRATION WAS DONE BUT UVH5's MISSING
             else:
                 no_uvh5=False
-                print(f"There are UVH5 files present in scan root directory: {root}")
+                logger.info(f"There are UVH5 files present in scan root directory: {root}")
                 number_of_uvh5_files = len([f for f in os.listdir(root) if (os.path.isfile(os.path.join(root, f)) and f.endswith('.uvh5'))])
-                print(f"Number of UVH's present: {number_of_uvh5_files}")
+                logger.debug(f"Number of UVH's present: {number_of_uvh5_files}")
 
             #Check for calibration gains in root
             if "calibration" in dirs and os.path.isdir(root + "/calibration/calibration_gains"):
-                print(f"There are JSON files present in scan root directory: {root}")
+                logger.info(f"There are JSON files present in scan root directory: {root}")
                 number_of_json_files = len([f for f in os.listdir(root+ "/calibration/calibration_gains")])
-                print(f"Number of JSON's present: {number_of_json_files}")
+                logger.debug(f"Number of JSON's present: {number_of_json_files}")
                 #We derived calibration solutions, collate.
-                print(f"{100*number_of_json_files/number_of_uvh5_files}% of the gains were generated from uvh5's.")
+                logger.info(f"{100*number_of_json_files/number_of_uvh5_files}% of the gains were generated from uvh5's.")
                 files_to_process = [f for f in os.listdir(root +"/calibration/calibration_gains/") if os.path.isfile(os.path.join(root +"/calibration/calibration_gains/", f))]
             else:
                 if no_uvh5:
-                    print(f"There are no UVH5 or JSON gains files present in scan root directory: {root}.\nUnable to recreate calibration run...")
+                    logger.error(f"There are no UVH5 or JSON gains files present in scan root directory: {root}.\nUnable to recreate calibration run...")
                     #LOG TO FILE THAT NO CALIBRATION RUN CAN BE REPLICATED - MISSING UVH5 AND JSON BUT PRODUCTS ARE PRESENT. 
                 else:
-                    print(f"There are no JSON files present in scan root directory: {root} but there are UVH5's. Recreating...")
-                    uvh5_command = f"/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 /home/cosmic/src/COSMIC-VLA-CalibrationEngine/calibrate_uvh5.py -d {root} -o {root + '/calibration'} --gengain"                                
-                    print(uvh5_command)
+                    logger.warning(f"There are no JSON files present in scan root directory: {root} but there are UVH5's. Recreating...")
+                    uvh5_command = f"/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 /home/cosmic/dev/COSMIC-VLA-CalibrationEngine/calibrate_uvh5.py -d {root} -o {root + '/calibration'} --gengain"                                
+                    logger.debug(f"Executing:\n{uvh5_command}")
+                    os.system(uvh5_command)
                     
             #Get some details from one of the json file names:
             observation_details_from_json = None
@@ -139,12 +140,14 @@ if __name__ == "__main__":
             t = datetime.datetime.strptime('Mon Apr 10 00:00:00 2023', "%a %b %d %H:%M:%S %Y")
             db_env = ''
             if mjd_time.unix < 1681084800.0:
-                db_env = 'COSMIC_DB_TABLE_SUFFIX=_before_202304010 '                
+                db_env = 'COSMIC_DB_TABLE_SUFFIX=_pre_202304010 '                
 
             #TEMPORARY
             sideband = '1 1'
             tbin = '1e-6'
             fcentmhz = '2477 3501'
-            collate_command = f"{db_env}/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 /home/cosmic/dev/COSMIC-VLA-DelayEngine/calibration_gain_collator.py {root + '/calibration/calibration_gains'+'/*.json'} -o {root + '/calibration'} --no-slack-post --fcentmhz {fcentmhz} --sideband {sideband} --snr-threshold 4.0 --tbin {tbin}  --start-epoch-seconds {mjd_time.unix} --cosmicdb-engine-configuration /home/cosmic/conf/cosmicdb_conf.yaml --dry-run"
-            print(collate_command)
+            # collate_command = f"{db_env}/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 /home/cosmic/dev/COSMIC-VLA-DelayEngine/calibration_gain_collator.py {root + '/calibration/calibration_gains'+'/*.json'} -o {root + '/calibration'} --no-slack-post --fcentmhz {fcentmhz} --sideband {sideband} --snr-threshold 4.0 --tbin {tbin}  --start-epoch-seconds {mjd_time.unix} --cosmicdb-engine-configuration /home/cosmic/conf/cosmicdb_conf.yaml --dry-run --archive-mode"
+            collate_command = f"/home/cosmic/anaconda3/envs/cosmic_vla/bin/python3 /home/cosmic/dev/COSMIC-VLA-DelayEngine/calibration_gain_collator.py {root + '/calibration/calibration_gains'+'/*.json'} -o {root + '/calibration'} --no-slack-post --fcentmhz {fcentmhz} --sideband {sideband} --snr-threshold 4.0 --tbin {tbin}  --start-epoch-seconds {mjd_time.unix} --dry-run --archive-mode"
+            logger.debug(f"Re-collating for retroarchival. Executing:\n{collate_command}")
+            os.system(collate_command)
             
